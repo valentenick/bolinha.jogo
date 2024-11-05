f58fcf5aa820aef82c29a37cd1acc3b26194ba5f
valentenick <nicole.valente.ramos@escola.pr.gov.br>
2024-11-05 12:02:39 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let cor;
+let posicaoHorizontal;
+let poicaoVertical;
+
+function setup() {
+  createCanvas(400, 400);
+  background("white");
+  cor = color (random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+ function draw() {
+  circle(posicaoHorizontal, posicaoVertical, 50);
+  fill(cor)
+  if(mouseX < posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal - 1;
+ 
+  }
+  if(mouseX > posicaoHorizontal){
+    posicaoHorizontal = posicaoHorizontal + 1;   
+  }  
+  if (mouseY < posicaoVertical){
+    posicaoVertical = posicaoVertical - 1;
+  }
+  if (mouseY > posicaoVertical){
+    posicaoVertical = posicaoVertical + 1; 
+  }
+  if(mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+}
